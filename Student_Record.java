6ac764ae69f0198d412f924956fb1a1b5ef33b7a
Pritam <pritamg06k@gmail.com>
2026-09-17 02:40:51 +0530

    Update and rename Student Record.java to Student_Record.java

--- Student_Record.java
+++ Student_Record.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
-public class s {
+public class Student_Record {
 
     static String saveFile = "C:\\JAVA VITyarthi\\studentdata.json";
     static String[] headers = { "id", "name", "age", "grade", "email", "address" };
